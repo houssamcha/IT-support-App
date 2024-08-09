c8d63e7547f5d808a807cfb0308041f2f6b01344
houssamcha <chagourihoussam@gmail.com>
2024-08-09 18:06:18 +0100

    Creation et modification de PanneService

--- IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/PanneService.java
+++ IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/PanneService.java
@@ -1,4 +1,11 @@
 package com.IT_support_App.IT_support_App.service;
 
+import com.IT_support_App.IT_support_App.model.Panne;
+import java.util.List;
+
 public interface PanneService {
+    Panne savePanne(Panne panne);
+    Panne getPanneById(Long id);
+    List<Panne> getAllPannes();
+    void deletePanne(Long id);
 }
