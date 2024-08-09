a58af4b987bf78fd816a04db253a547be12290c8
houssamcha <chagourihoussam@gmail.com>
2024-08-09 11:01:07 +0100

    Creation du l'interface PersonneService

--- IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/EquipementService.java
+++ IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/EquipementService.java
@@ -1,4 +1,5 @@
 package com.IT_support_App.IT_support_App.service;
 
 public interface EquipementService {
+
 }
--- IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/PersonneService.java
+++ IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/PersonneService.java
@@ -1,2 +1,11 @@
-package com.IT_support_App.IT_support_App.service;public interface PersonneService {
+package com.IT_support_App.IT_support_App.service;
+
+import com.IT_support_App.IT_support_App.model.Personne;
+import java.util.List;
+
+public interface PersonneService {
+    Personne savePersonne(Personne personne);
+    Personne getPersonneById(Long id);
+    List<Personne> getAllPersonnes();
+    void deletePersonne(Long id);
 }
