a07c97e3d8d6891ac2df851580bf179f335ebceb
houssamcha <chagourihoussam@gmail.com>
2024-08-09 18:49:42 +0100

    Creation d'un dossier Admin avec un composant des equipement

--- FrontEnd IT-support-App/IT-support-app/src/app/app.module.ts
+++ FrontEnd IT-support-App/IT-support-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquipementComponent } from './Admin/equipement/equipement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipementComponent
   ],
   imports: [
     BrowserModule,
