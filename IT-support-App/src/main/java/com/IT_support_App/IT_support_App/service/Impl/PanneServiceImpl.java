2163ce25ec1ec525a18cae0e9d60fd82842f9987
houssamcha <chagourihoussam@gmail.com>
2024-08-09 18:07:08 +0100

    Creation et modification de PanneServiceImpl

--- IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/Impl/PanneServiceImpl.java
+++ IT-support-App/src/main/java/com/IT_support_App/IT_support_App/service/Impl/PanneServiceImpl.java
@@ -1,7 +1,38 @@
 package com.IT_support_App.IT_support_App.service.Impl;
 
+import com.IT_support_App.IT_support_App.model.Panne;
+import com.IT_support_App.IT_support_App.repository.PanneRepository;
 import com.IT_support_App.IT_support_App.service.PanneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PanneServiceImpl implements PanneService {
 
+    @Autowired
+    private PanneRepository panneRepository;
+
+    @Override
+    public Panne savePanne(Panne panne) {
+        return panneRepository.save(panne);
+    }
+
+    @Override
+    public Panne getPanneById(Long id) {
+        Optional<Panne> optionalPanne = panneRepository.findById(id);
+        return optionalPanne.orElse(null); // Ou lancer une exception si souhaité
+    }
+
+    @Override
+    public List<Panne> getAllPannes() {
+        return panneRepository.findAll();
+    }
+
+    @Override
+    public void deletePanne(Long id) {
+        panneRepository.deleteById(id);
+    }
 }
