ff32226c994fecffe92bed777e81cc9a4c5379a7
houssamcha <chagourihoussam@gmail.com>
2024-08-09 18:13:00 +0100

    Modification du PanneController CRUD

--- IT-support-App/src/main/java/com/IT_support_App/IT_support_App/Controller/PanneController.java
+++ IT-support-App/src/main/java/com/IT_support_App/IT_support_App/Controller/PanneController.java
@@ -1,2 +1,36 @@
-package com.IT_support_App.IT_support_App.Controller;public class PanneController {
+package com.IT_support_App.IT_support_App.Controller;
+
+import com.IT_support_App.IT_support_App.model.Panne;
+import com.IT_support_App.IT_support_App.service.PanneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pannes")
+public class PanneController {
+
+    @Autowired
+    private PanneService panneService;
+
+    @GetMapping
+    public List<Panne> getAllPannes() {
+        return panneService.getAllPannes();
+    }
+
+    @PostMapping
+    public Panne createPanne(@RequestBody Panne panne) {
+        return panneService.savePanne(panne);
+    }
+
+    @GetMapping("/{id}")
+    public Panne getPanneById(@PathVariable Long id) {
+        return panneService.getPanneById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePanne(@PathVariable Long id) {
+        panneService.deletePanne(id);
+    }
 }
